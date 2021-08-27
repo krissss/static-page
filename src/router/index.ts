39f03993bbd0ae81bd0f33561ad1fcf87c2fa7a6
kriss <462679766@qq.com>
2021-08-27 13:29:33 +0800

    feat: clean

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,6 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/qnyh-keju',
     name: 'qnyh-keju',
     component: () => import(/* webpackChunkName: "qnyh-keju" */ '../views/qnyh/KeJu.vue')
